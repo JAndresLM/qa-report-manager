1292033555669567f9174c3d971752e8c453242e
Andres Lopez <josandlopmol@hotmail.com>
2018-11-07 16:02:56 -0600

    Removing unnecesary code

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { jobsData}    from './jobs/jobs.component';
 
 @Injectable({
@@ -13,12 +12,10 @@ export class DataService {
 
   getJobs() {
     return this.http.get('https://ghibliapi.herokuapp.com/films');
-    //return this.http.get('http://dev-jenkins.spongecell.net/view/QA%20-%20Full%20Regression/api/json?tree=jobs[name,color,builds[id,fullDisplayName,result,url,timestamp,duration,actions[failCount,totalCount]{8,9}]]')
   }
 
   getJobs2(){
     return this.http.get<jobsData[]>('https://ghibliapi.herokuapp.com/films');
-    //return this.http.get('http://dev-jenkins.spongecell.net/view/QA%20-%20Full%20Regression/api/json?tree=jobs[name,color,builds[id,fullDisplayName,result,url,timestamp,duration,actions[failCount,totalCount]{8,9}]]')
   }
 
   getBuilds() {
--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
-import { Observable } from 'rxjs';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 
@@ -11,7 +10,7 @@ import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 })
 
 export class JobsComponent implements OnInit {
-  displayedColumns = ['id', 'title', 'description', 'director'];
+  displayedColumns = ['status', 'name', 'last_execution', 'duration', 'results', 'actions'];
   dataSource: MatTableDataSource<jobsData>;
 
   jobs$: Object;
@@ -20,20 +19,9 @@ export class JobsComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private data: DataService) {
-    /*this.jobsListData = [
-      {
-        id: "1",
-        title: "1",
-        description: "1",
-        director: "1",
-      }
-    ]
-    this.dataSource = new MatTableDataSource(this.jobsListData);*/
-  }
+  constructor(private data: DataService) {}
 
   ngOnInit() {
-    console.log("On init");
     this.data.getJobs2().subscribe(
       data => {
         this.jobs$ = data;
@@ -45,14 +33,9 @@ export class JobsComponent implements OnInit {
     );
   }
 
-  ngAfterViewInit() {
-    //this.dataSource.paginator = this.paginator;
-    //this.dataSource.sort = this.sort;
-  }
-
   applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
   }
 
