3c66dbff2b0e3dcf28c7b7a2796577621eba122d
Andres Lopez <josandlopmol@hotmail.com>
2018-11-12 12:49:26 -0600

    UI changes and dummy graph

--- src/app/builds/builds.component.ts
+++ src/app/builds/builds.component.ts
@@ -1,6 +1,19 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
-import { DataService } from '../data.service';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {
+  Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {
+  DataService
+} from '../data.service';
+import {
+  MatPaginator,
+  MatSort,
+  MatTableDataSource
+} from '@angular/material';
+import {
+  Chart
+} from 'chart.js';
 
 @Component({
   selector: 'app-builds',
@@ -11,15 +24,45 @@ import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 export class BuildsComponent implements OnInit {
 
   displayedColumns = ['status', 'build_number', 'date', 'duration', 'total_tcs', 'passed_tcs', 'failed_tcs', 'pass_rate', 'reports'];
-  dataSource: MatTableDataSource<buildsData>;
+  dataSource: MatTableDataSource < buildsData > ;
+
+  timeLapses: timeLapse[] = [{
+      value: 'allTime',
+      viewValue: 'All Time'
+    },
+    {
+      value: 'lastYear',
+      viewValue: 'Last Year'
+    },
+    {
+      value: 'lastMonth',
+      viewValue: 'Last Month'
+    },
+    {
+      value: 'lastWeek',
+      viewValue: 'Last Week'
+    },
+    {
+      value: 'lastDay',
+      viewValue: 'Last Day'
+    },
+    {
+      value: 'today',
+      viewValue: 'Today '
+    }
+  ];
+
+  group_value: string = "table";
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   builds$: Object;
-  buildsListData:buildsData[] = [];
+  buildsListData: buildsData[] = [];
 
-  constructor(private data: DataService) { }
+  chart = [];
+
+  constructor(private data: DataService) {}
 
   ngOnInit() {
     this.data.getBuilds2().subscribe(
@@ -29,7 +72,8 @@ export class BuildsComponent implements OnInit {
         this.dataSource = new MatTableDataSource(this.buildsListData);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
-      } 
+        this.createChart();
+      }
     );
   }
 
@@ -37,6 +81,62 @@ export class BuildsComponent implements OnInit {
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
+  }
+
+  selectTableView() {
+    this.group_value = "table";
+  }
+
+  selectGraphView() {
+    this.group_value = "graph";
+  }
+
+  isGraphDisplayed(): boolean {
+    var isDisplayed = this.group_value === "graph";
+    //console.log(isDisplayed);
+    return isDisplayed;
+  }
+
+  createChart() {
+    var labels = ["Nov 01", "Nov 02", "Nov 03", "Nov 04", "Nov 05", "Nov 06", "Nov 07", "Nov 08", "Nov 09", "Nov 10", "Nov 11", "Nov 12"]
+    var passing = [40, 90, 70, 98, 63, 45, 85, 30, 21, 85, 94, 67]
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Pass Rate Percentage (%)",
+          data: passing,
+          borderColor: "#e67fb9",
+          backgroundColor: "#f7d4e5",
+          fill: true
+        }, ]
+      },
+      options: {
+        legend: {
+          display: true
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              beginAtZero: true,
+              steps: 5,
+              stepValue: 5,
+              max: 100
+            }
+          }],
+        },
+        elements: {
+          line: {
+            tension: 0,
+          }
+        }
+      }
+    });
   }
 
 }
@@ -51,4 +151,9 @@ export interface buildsData {
   failed_tcs: string;
   pass_rate: string;
   reports: string;
+}
+
+export interface timeLapse {
+  value: string;
+  viewValue: string;
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule} from '@angular/material';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MatFormFieldModule, MatInputModule, MatPaginatorModule, MatTableModule, MatSortModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTableModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTableModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonToggleModule,
+    MatSelectModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonToggleModule,
+    MatSelectModule,
+    MatCheckboxModule
+  ],
 })
 export class MaterialModule { }
